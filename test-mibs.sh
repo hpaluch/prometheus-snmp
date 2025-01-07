7a279e55d058a4651a43854e5cce4544f957bd33
Henryk Paluch <hpaluch@seznam.cz>
2025-01-07 17:46:29 +0100

    Added hrTables (cross-platform) to test script

--- test-mibs.sh
+++ test-mibs.sh
@@ -16,23 +16,29 @@ function verbose_cmd
 
 # test scalar MIBs
 echo "* Testing scalar MIB(s)..."
-for mib in DISMAN-EVENT-MIB::sysUpTimeInstance
+# NOTE: HOST-RESROUCES-MIB is well supported on both Linux and plain Windows SNMP service:
+for mib in HOST-RESOURCES-MIB::hrSystem
 do
 	verbose_cmd snmpwalk -Os $cmd_common $mib | tee $tmp
 	# 'wc -l' prints: COUNTER FILENAME
 	lines=$(wc -l $tmp | awk '{print $1}')
-	exp_lines=1
-	[ "$lines" -eq $exp_lines ] || {
-		echo "ERROR: Got $lines lines for MIB $mib, but expected $exp_lines" >&2
+	min_lines=1
+	[ $lines -ge $min_lines ] || {
+		echo "ERROR: Got $lines lines for MIB $mib, but expected at least $min_lines" >&2
 		exit 1
 	}
-	echo "OK (exp lines $exp_lines, got lines $lines)"
+	echo "OK (exp min lines $min_lines, got lines $lines)"
 done
 
 # test tables - Disks (dskTable), Load Averages (laTable), Network interfaces (ifTable):
 echo
 echo  "* Testing table MIB(s)..."
-for mib in UCD-SNMP-MIB::ucdavis.dskTable UCD-SNMP-MIB::ucdavis.laTable IF-MIB::ifTable
+# these tables are supported by UCD SNMP only (Unix)
+declare -a ucd_tables=(UCD-SNMP-MIB::ucdavis.dskTable UCD-SNMP-MIB::ucdavis.laTable IF-MIB::ifTable)
+# hrTables are supported by both UCD SNMP and Windows
+declare -a hr_tables=(HOST-RESOURCES-MIB::hrStorageTable HOST-RESOURCES-MIB::hrDeviceTable
+	HOST-RESOURCES-MIB::hrProcessorTable HOST-RESOURCES-MIB::hrNetworkTable HOST-RESOURCES-MIB::hrFSTable)
+for mib in "${ucd_tables[@]}" "${hr_tables[@]}"
 do
 	verbose_cmd snmptable -Cb $cmd_common $mib | tee $tmp
 	# 'wc -l' prints: COUNTER FILENAME
